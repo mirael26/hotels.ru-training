1c5ae25c536503302c3bc145db44efc3c1a272f2
Elmira Ilyasova <capricorn_26@mail.ru>
2022-06-25 03:07:27 +0700

    2.17 tasks - chapter 1

---
+++ learn.javascript/chapter 1/2.17.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Перепишите с использованием функции-стрелки
+
+function ask(question, yes, no) {
+  if (confirm(question)) yes()
+  else no();
+}
+
+ask(
+  "Вы согласны?",
+  () => alert("Вы согласились."),
+  () => alert("Вы отменили выполнение.")
+);
