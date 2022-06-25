5ce9d7f021ab7f96477e5983fc2f4cf46df9a380
Elmira Ilyasova <capricorn_26@mail.ru>
2022-06-25 22:27:04 +0700

    5.3 tasks - chapter 1

---
+++ learn.javascript/chapter 1/5/5.3.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// Сделать первый символ заглавным
+
+const ucFirst = (str) => {
+  return (str) ? str[0].toUpperCase + str.slice(1, str.length) : str;
+};
+
+// Проверка на спам
+
+const checkSpam = (str) => {
+  return (str.toLowerCase.contains('viagra') || str.toLowerCase.contains('xxx'));
+};
+
+// Усечение строки
+
+const truncate = (str, maxlength) => {
+  return (str.length <= maxlength) ? str : (str.slice(0, (maxlength - 1)) + '…');
+};
+
+// Выделить число
+
+const extractCurrencyValue = (str) => {
+  if (str[0] === '$' || isFinite(str.slice(1))) {
+    return +str.slice(1);
+  }
+  return str;
+};
