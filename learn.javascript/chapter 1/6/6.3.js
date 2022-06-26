b31e29d68510d032fcef7a6f5d4cdc1d37bc8a69
Elmira Ilyasova <capricorn_26@mail.ru>
2022-06-27 00:55:12 +0700

    6.3 tasks  - chapter 1

---
+++ learn.javascript/chapter 1/6/6.3.js
@@ -0,0 +1,53 @@
+'use strict';
+
+// Сумма с помощью замыканий
+
+const sum = (a) => {
+  let c = 0;
+
+  return (b) => {
+    return a + b;
+  }
+};
+
+console.log(sum(4)(5))
+
+// Фильтрация с помощью функции
+
+const inBetween = (a, b) => {
+  return (element) => {
+    return (element >= a && element <= b);
+  }
+};
+
+const inArray = (arr) => {
+  return (element) => {
+    return arr.includes(element);
+  }
+};
+
+// Сортировать по полю
+
+const byField = (field) => {
+  return (a, b) => {
+    return a[field] > b[field] ? 1 : -1;
+  };
+};
+
+// Армия функций
+
+function makeArmy() {
+  let shooters = [];
+
+  let i = 0;
+  while (i < 10) {
+    let j = i;
+    let shooter = function() { // функция shooter
+      alert( j ); // должна выводить порядковый номер
+    };
+    shooters.push(shooter);
+    i++;
+  }
+
+  return shooters;
+}
