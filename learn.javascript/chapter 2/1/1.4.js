7c3b7cce1dbd3859241d5cd2778da3910556fab6
Elmira Ilyasova <capricorn_26@mail.ru>
2022-06-27 18:24:51 +0700

    1.4 tasks - chapter 2

---
+++ learn.javascript/chapter 2/1/1.4.js
@@ -0,0 +1,11 @@
+// Поиск элементов
+
+let table = document.querySelector("#age-table");
+document.querySelectorAll('label');
+table.querySelector('td');
+let form = document.querySelector('form[name="search"]');
+
+let inputs = form.querySelectorAll('input')
+
+inputs[0];
+inputs[inputs.length-1]
